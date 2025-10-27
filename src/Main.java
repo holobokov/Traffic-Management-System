5d2e5ff008550d6c80bf3fee20bfa6cecae00181
holobokov <holobokovo@gmail.com>
2025-10-27 17:32:43 +0100

    Starting project

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("Welcome to the Traffic Management System!");
+        System.out.println("Menu:");
+        System.out.println("1. Add");
+        System.out.println("2. Delete");
+        System.out.println("3. System");
+        System.out.println("0. Quit");
+    }
+}
+
