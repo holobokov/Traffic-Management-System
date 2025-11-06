f1eed4cd321e38f1b42889c9a048d2193fb30ae7
holobokov <holobokovo@gmail.com>
2025-11-06 18:22:12 +0100

    zaimplementowano generacje ID w intersections, dodano funkcje printIntersection

--- src/Intersection.java
+++ src/Intersection.java
@@ -27,11 +27,11 @@ public class Intersection {
         intersections.add(this);
     }
 
-    public Intersection(String id, String name, double lat, double lon,
+    public Intersection(String name, double lat, double lon,
                         ArrayList<Direction> availableDirections,
                         ArrayList<TrafficLights> trafficLights,
                         int vehiclesWaiting) {
-        this.id = id;
+        this.id = generateId();
         this.name = name;
         this.lat = lat;
         this.lon = lon;
@@ -46,24 +46,52 @@ public class Intersection {
         intersections.add(this);
     }
 
+    private String generateId() {
+        Random rand = new Random();
+        String newId;
+
+        do {
+            int id = rand.nextInt(100000);
+            newId = "INT-" + id;
+        } while (idExists(newId));
+
+        return newId;
+    }
+
+    private boolean idExists(String id) {
+        for (Intersection inters: intersections) {
+            if (inters.id.equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void addVehicle(Car vehicle, Direction direction) {
+        if (!trafficLightsByDirection.containsKey(direction)) return;
         if (!vehiclesByDirection.containsKey(direction)) vehiclesByDirection.put(direction, new ArrayList<>());
         vehiclesByDirection.get(direction).add(vehicle);
+        vehiclesWaiting++;
     }
 
     public void showVehicles() {
-        for (Direction dir: vehiclesByDirection.keySet()) {
-            int c = 0;
-            int b = 0;
-            int e = 0;
-            for (Car car : vehiclesByDirection.get(dir)) {
-                if (car.getPriority() == 1) c++;
-                else if (car.getPriority() == 3) b++;
-                else if (car.getPriority() == 10) e++;
-            }
-
-            System.out.println("Direction: " + dir + ", cars: " + c + ", buses: " + b + ", emergency vehicles:" + e);
+        for (Direction dir : vehiclesByDirection.keySet()) {
+            int[] vehicles = getVehiclesByDirection(dir);
+            System.out.println("Direction: " + dir + ", cars: " + vehicles[0] + ", buses: " + vehicles[1] + ", emergency vehicles:" + vehicles[2]);
         }
+    }
+
+    public int[] getVehiclesByDirection(Direction direction) {
+        int c = 0;
+        int b = 0;
+        int e = 0;
+        for (Car car : vehiclesByDirection.get(direction)) {
+            if (car.getPriority() == 1) c++;
+            else if (car.getPriority() == 3) b++;
+            else if (car.getPriority() == 10) e++;
+        }
+
+        return new int[]{c, b, e};
     }
 
 
@@ -109,6 +137,31 @@ public class Intersection {
 
     public String getId() {
         return id;
+    }
+
+    public void printIntersection() {
+        int[] north = trafficLightsByDirection.containsKey(Direction.north) ? getVehiclesByDirection(Direction.north) : new int[]{0, 0, 0};
+        int[] south = trafficLightsByDirection.containsKey(Direction.south) ? getVehiclesByDirection(Direction.south) : new int[]{0, 0, 0};
+        int[] west = trafficLightsByDirection.containsKey(Direction.west) ? getVehiclesByDirection(Direction.west) : new int[]{0, 0, 0};
+        int[] east = trafficLightsByDirection.containsKey(Direction.east) ? getVehiclesByDirection(Direction.east) : new int[]{0, 0, 0};
+        
+        System.out.println("\n========================================");
+        System.out.println("  INTERSECTION: " + name + " (ID: " + id + ")");
+        System.out.println("========================================");
+        System.out.println();
+        System.out.println("                   NORTH");
+        System.out.println("                 C:" + north[0] + " B:" + north[1] + " E:" + north[2]);
+        System.out.println("                     |");
+        System.out.println("                     |");
+        System.out.println(" WEST C:" + west[0] + " B:" + west[1] + " E:" + west[2] + "--- + ---E:" + east[2] + " B:" + east[1] + " C:" + east[0] + " EAST");
+        System.out.println("                     |");
+        System.out.println("                     |");
+        System.out.println("                 C:" + south[0] + " B:" + south[1] + " E:" + south[2]);
+        System.out.println("                   SOUTH");
+        System.out.println();
+        System.out.println("Total waiting vehicles: " + vehiclesWaiting);
+        System.out.println("Legend: C=Cars, B=Buses, E=Emergency");
+        System.out.println("========================================\n");
     }
 
     @Override
--- src/Main.java
+++ src/Main.java
@@ -7,14 +7,15 @@ public class Main {
         ArrayList<TrafficLights> trfc = new ArrayList<TrafficLights>();
         Collections.addAll(trfc, new TrafficLights(Light.red, Direction.north, WorkingMode.auto),
                 new TrafficLights(Light.green, Direction.east, WorkingMode.auto),
-                new TrafficLights(Light.green, Direction.west, WorkingMode.auto));
+                new TrafficLights(Light.green, Direction.west, WorkingMode.auto)
+        );
 
         Map<Direction, Car> vehiclesByDirection = new HashMap<Direction, Car>();
 
 
-        Intersection intersection = new Intersection("318935", "Marszałkowska x Warszawska", 93.22, 224.1, directions, trfc, 3);
-        Intersection intersection2 = new Intersection("318936", "Mikołowska x Karola Miarki", 53.22, 224.1, directions, trfc, 3);
-        Intersection intersection3 = new Intersection("318937", "Mickiewicza x 3 maja", 97.22, 224.1, directions, trfc, 3);
+        Intersection intersection = new Intersection("Marszałkowska x Warszawska", 93.22, 224.1, directions, trfc, 3);
+        Intersection intersection2 = new Intersection("Mikołowska x Karola Miarki", 53.22, 224.1, directions, trfc, 3);
+        Intersection intersection3 = new Intersection("Mickiewicza x 3 maja", 97.22, 224.1, directions, trfc, 3);
 
 //        Intersection.printIntersections();
 
@@ -22,11 +23,12 @@ public class Main {
         Bus bus = new Bus();
         EmergencyVehicle ambulance = new EmergencyVehicle();
 
-        intersection.addVehicle(car, Direction.east);
-        intersection.addVehicle(bus, Direction.east);
         intersection.addVehicle(ambulance, Direction.north);
+        intersection.addVehicle(car, Direction.west);
+        intersection.addVehicle(bus, Direction.east);
 
         intersection.showVehicles();
+        intersection.printIntersection();
     }
 
     public static void greetings() {
